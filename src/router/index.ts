aa27eb642b4638bd70a8565d6f1b8d1e32e65d5e
miaojing <a1024621284@163.com>
2021-06-09 18:25:27 +0800

    '添加表单校验'

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,16 +4,18 @@ import Home from '../views/Home.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: () => import('../views/index.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/loginRegister.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: '/404',
+    component: () => import('../views/404.vue')
   }
 ]
 
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    首页
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name:"Index",
+  components:{
+    
+  }
+};
+</script>
+
+<style scoped>
+</style>
